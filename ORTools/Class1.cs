6d994a10d3826bb1f94b9696d9ff7c31c0cb4087
Simon THUAUD <simounixx@gmail.com>
2024-03-13 15:28:02 +0100

    init project for ORTools

---
+++ ORTools/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace ORTools;
+
+public class Class1
+{
+}
---
+++ Sudoku.ORTools/ORToolsSolverV1.cs
@@ -0,0 +1,11 @@
+﻿using Sudoku.Shared;
+
+namespace Sudoku.ORTools;
+
+public class ORToolsSolverV1 : ISudokuSolver
+{
+    public SudokuGrid Solve(SudokuGrid s)
+    {
+        return s;
+    }
+}
