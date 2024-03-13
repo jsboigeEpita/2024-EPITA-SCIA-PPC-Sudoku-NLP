0ea65762fc9c99780b9c74dedb48a1a20d998cab
Simon THUAUD <simounixx@gmail.com>
2024-03-13 15:41:51 +0100

    fix: changed filename for linear solver

---
+++ Sudoku.ORTools/ORToolsLinearSolver.cs
@@ -0,0 +1,19 @@
+﻿using Sudoku.Shared;
+using Google;
+using Google.OrTools.LinearSolver;
+
+namespace Sudoku.ORTools;
+
+public class ORToolsLinearSolver : ISudokuSolver
+{
+    public SudokuGrid Solve(SudokuGrid s)
+    {
+        Solver solver = Solver.CreateSolver("GLOP");
+        if (solver is null)
+        {
+            return s; // Error loading the solver
+        }
+
+        return s;
+    }
+}
