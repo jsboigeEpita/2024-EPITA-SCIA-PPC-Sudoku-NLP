a45d2b149e4ae7b12102b9b6eba148f92d61fe3b
lyes <lyes.benacer@epita.fr>
2024-03-24 20:37:46 +0100

    Add loop with size that increase

--- Sudoku.Shared/EmptySolver.cs
+++ Sudoku.Shared/EmptySolver.cs
@@ -6,10 +6,10 @@ namespace Sudoku.Shared
 {
     public class EmptySolver : ISudokuSolver
     {
+        int population_size = 1000;
+        
         public SudokuGrid Solve(SudokuGrid s)
         {
-
-
             var cleanSudoku = new SudokuCleaning(s);
             Dictionary<int, List<int>> mask = cleanSudoku.mask; 
             SudokuGrid sudoku = cleanSudoku.sudoku;
@@ -19,22 +19,31 @@ namespace Sudoku.Shared
             var mutation = new UniformMutation();
             var fitness = new SudokuFitness();
             var chromosome = new SudokuChromosome(sudoku);
-            var population = new Population(10000, 30000, chromosome);
-            
-            var ga = new GeneticAlgorithm(population, fitness, selection, crossover, mutation);
-            ga.Termination = new GenerationNumberTermination(100);
             
             Console.WriteLine("GA running...");
-            ga.GenerationRan += (sender, e) =>
+            while (true)
             {
-                var bestFitness = ga.BestChromosome.Fitness;
-                Console.WriteLine($"Generation {ga.GenerationsNumber}: Best Fitness = {bestFitness}");
-            };
-            ga.Start();
+                var population = new Population(population_size, population_size, chromosome);
+                var ga = new GeneticAlgorithm(population, fitness, selection, crossover, mutation);
+                ga.Termination = new OrTermination(new FitnessStagnationTermination(20), new FitnessThresholdTermination(1));
+                // ga.OperatorsStrategy = new TplOperatorsStrategy();
+	            // ga.TaskExecutor = new TplTaskExecutor();
 
-            Console.WriteLine("Best solution found has {0} fitness.", ga.BestChromosome.Fitness);
-            var sudokuChromosome = ga.BestChromosome as SudokuChromosome;
-            return sudokuChromosome.getSolution();
+                ga.GenerationRan += (sender, e) =>
+                {
+                    var bestFitness = ga.BestChromosome.Fitness;
+                    Console.WriteLine($"Generation {ga.GenerationsNumber}: Best Fitness = {bestFitness}");
+                };
+
+                ga.Start();
+
+                if (ga.BestChromosome.Fitness == 1)
+                {
+                    var sudokuChromosome = ga.BestChromosome as SudokuChromosome;
+                    return sudokuChromosome.getSolution();
+                }
+                population_size = (int)Math.Round(population_size * 1.5);
+            }
         }
     }
 }
