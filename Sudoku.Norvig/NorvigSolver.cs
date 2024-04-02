324811f5a5a72e6c4293214178a076a968e37871
Lilian Schall <lilian.schall@epita.fr>
2024-04-02 20:04:19 +0200

    feat: finished development of the norvig algorithm

--- Sudoku.Norvig/NorvigSolver.cs
+++ Sudoku.Norvig/NorvigSolver.cs
@@ -8,51 +8,111 @@ public class NorvigSolver : ISudokuSolver
     private short[] _possibleValues; // a 1D array that represent all possible value of a given cell of the sudoku
 
     private HashSet<int>[,] _units;
-    
+    private HashSet<int>[] _peers;
+
     public NorvigSolver()
     {
         _possibleValues = new short[Tools.SURFACE];
         _units = new HashSet<int>[Tools.SURFACE, 3];
+        _peers = new HashSet<int>[Tools.SURFACE];
         Tools.FillUnits(_units);
-        Parallel.For(0, Tools.SURFACE, (i, state) => _possibleValues[i] = 0x1FF);
+        Tools.FillPeers(_peers);
     }
 
     public SudokuGrid Solve(SudokuGrid s)
     {
-        RemoveInitialPossibleValues(s);
-        for (int i = 0; i < Tools.SIZE; i++)
+        Parallel.For(0, Tools.SURFACE, (i, state) => _possibleValues[i] = 0x1FF);
+        Constrain(s);
+
+        if (!NoMorePossibleValues())
         {
-            for (int j = 0; j < Tools.SIZE; j++)
-            {
-            }
+            Search();
         }
 
+        FillGrid(s);
         return s;
     }
 
-    private void RemoveInitialPossibleValues(SudokuGrid sudokuGrid)
+    private bool Search()
+    {
+        
+        // we search the cell that has the least possible value
+        int cell = FindCellWithLeastPossibilities();
+
+        // if no cell has multiple possibilites, the search has succeeded
+        if (cell == -1)
+            return true;
+
+        for (short i = 1; i < 0x1FF; i = (short)(i << 1))
+        {
+            if ((_possibleValues[cell] & i) == 0)
+                continue;
+
+            int digit = Tools.ConvertBitwiseToDecimal(i);
+
+            short[] save = (short[])_possibleValues.Clone();
+
+            Fill(cell, digit);
+            bool hasSucceeded = Search();
+            if (hasSucceeded)
+                return true;
+            
+            // else we use our save to restate our _possible values to the original
+            _possibleValues = save;
+        }
+
+        return false;
+    }
+
+    private int FindCellWithLeastPossibilities()
+    {
+        uint lowestNbBitsSet = 9;
+        int highestCell = -1;
+        for (int i = 0; i < Tools.SURFACE; i++)
+        {
+            uint setBits = System.Runtime.Intrinsics.X86.Popcnt.PopCount((uint)_possibleValues[i]);
+
+            if (setBits < lowestNbBitsSet && setBits != 1)
+            {
+                highestCell = i;
+                lowestNbBitsSet = setBits;
+            }
+        }
+
+        return highestCell;
+    }
+
+    private bool NoMorePossibleValues()
+    {
+        for (int i = 0; i < Tools.SURFACE; i++)
+            if (!Tools.IsOnlyOneBitSet(_possibleValues[i]))
+                return false;
+
+        return true;
+    }
+
+    private void FillGrid(SudokuGrid s)
+    {
+        for (int i = 0; i < Tools.SIZE; i++)
+        for (int j = 0; j < Tools.SIZE; j++)
+        {
+            if (!Tools.IsOnlyOneBitSet(_possibleValues[i * Tools.SIZE + j]))
+                s.Cells[i, j] = 0;
+            else
+                s.Cells[i, j] = Tools.ConvertBitwiseToDecimal(_possibleValues[i * Tools.SIZE + j]);
+        }
+    }
+
+    private void Constrain(SudokuGrid s)
     {
         for (int i = 0; i < Tools.SIZE; i++)
         {
             for (int j = 0; j < Tools.SIZE; j++)
             {
-                int cellValue = sudokuGrid.Cells[i, j];
-
-                if (cellValue == 0)
-                    continue;
-
-                short bitmask = (short)(0x1 << (cellValue - 1));
-                _possibleValues[i * Tools.SIZE + j] = bitmask;
-
-                var neighbours = SudokuGrid.CellNeighbours[i][j];
-
-                short inverseBitmask = (short)(bitmask ^ 0x1FF);
-
-                foreach ((int row, int column) neighbour in neighbours)
+                int cell = i * Tools.SIZE + j;
+                if (s.Cells[i, j] != 0)
                 {
-                    short buffer = _possibleValues[neighbour.row * Tools.SIZE + neighbour.column];
-
-                    _possibleValues[neighbour.row * Tools.SIZE + neighbour.column] = (short)(buffer & inverseBitmask);
+                    Fill(cell, s.Cells[i, j]);
                 }
             }
         }
@@ -60,49 +120,72 @@ public class NorvigSolver : ISudokuSolver
 
     private bool Fill(int cell, int digit)
     {
-        throw new NotImplementedException();
-    }
+        if (Tools.IsOnlyOneBitSet(_possibleValues[cell]) && Tools.ConvertBitwiseToDecimal(_possibleValues[cell]) == digit)
+            return true;
 
+        bool eliminated = true;
+
+        for (int newDigit = 1; newDigit < 10; newDigit++)
+        {
+            if (newDigit == digit)
+                continue;
+
+            eliminated = Eliminate(cell, newDigit) && eliminated;
+        }
+
+        return eliminated;
+    }
+    
     private bool Eliminate(int cell, int digit)
     {
+        // bitmask in order to select the (digit - 1) bit
         short bitmask = (short)(0x1 << (digit - 1));
-        
+
+        // if we already eliminated the digit, we return true
+        // as to say we successfully eliminated it already
         if ((_possibleValues[cell] & bitmask) == 0)
             return true;
-        
-        short inverseBitmask = (short)(bitmask ^ 0x1FF);
-        short buffer = (short)(_possibleValues[cell] & inverseBitmask);
 
-        if (buffer == 0)
+        short inverseBitmask = (short)(bitmask ^ 0x1FF);
+        
+        // we eliminate the bit that represents `digit`
+        short newPossibleValues = (short)(_possibleValues[cell] & inverseBitmask);
+
+        _possibleValues[cell] = newPossibleValues;
+        
+        // if there is no possible value left
+        // that is not a legal elimination
+        if (newPossibleValues == 0)
             return false;
 
-        short[] save = (short[])_possibleValues.Clone();
-        _possibleValues[cell] = buffer;
-        
-        if ((buffer & (buffer - 1)) == 0)
+
+        // if there is only one possibility
+        // we eliminate that possibility for each of the cell's peers
+        if (Tools.IsOnlyOneBitSet(newPossibleValues))
         {
-            var neighbours = SudokuGrid.CellNeighbours[cell / Tools.SIZE][cell % Tools.SIZE];
+            var peers = _peers[cell];
 
-            int digit2 = int.TrailingZeroCount(buffer) + 1;
+            int digit2 = Tools.ConvertBitwiseToDecimal(newPossibleValues);
 
-            foreach ((int row, int column) neighbour in neighbours)
+            bool eliminated = true;
+
+            foreach (int cellNeighbour in peers)
             {
-                if (!Eliminate(neighbour.row * Tools.SIZE + neighbour.column, digit2))
-                {
-                    _possibleValues = save;
-                    return false;
-                }
+                eliminated = Eliminate(cellNeighbour, digit2) && eliminated;
             }
+
+            if (!eliminated)
+                return false;
         }
 
         for (int i = 0; i < 3; i++)
         {
             HashSet<int> unit = _units[cell, i];
-            IEnumerable<int> possibleDigitPlaces = unit.Where(x => (inverseBitmask & _possibleValues[x]) != 0);
-            if (possibleDigitPlaces.Any() ||
+            // construction of the list of units that has `digit` as a possibility
+            IEnumerable<int> possibleDigitPlaces = unit.Where(x => (bitmask & _possibleValues[x]) != 0);
+            if (!possibleDigitPlaces.Any() ||
                 (possibleDigitPlaces.Count() == 1 && !Fill(possibleDigitPlaces.First(), digit)))
             {
-                _possibleValues = save;
                 return false;
             }
         }
--- Sudoku.Norvig/Tools.cs
+++ Sudoku.Norvig/Tools.cs
@@ -37,18 +37,86 @@ public class Tools
         });
     }
 
+    public static string Picture(short[] possibleValues)
+    {
+        int maxWidth = 0;
+        for (int i = 0; i < SURFACE; i++)
+        {
+            int width = CellToString(possibleValues[i]).Length;
+            if (width > maxWidth)
+                maxWidth = width;
+        }
+
+        string dash1 = "";
+        for (int i = 0; i < (maxWidth * 3 + 2); i++)
+            dash1 += "-";
+
+        string dash3 = "\n";
+        for (int i = 0; i < 3; i++)
+            dash3 += dash1 + (i == 2 ? "" : "+");
+
+        string buffer = "";
+
+        for (int i = 0; i < SURFACE; i++)
+        {
+            if (i != 0 && i % 27 == 0)
+                buffer += dash3;
+            buffer += (i % 3 == 0 && i % 9 != 0 ? "|" : i % 9 == 0 ? "\n" : "") +
+                      CenterString(CellToString(possibleValues[i]), maxWidth);
+        }
+
+        return buffer;
+    }
+
+    private static string CellToString(short possibleValue)
+    {
+        if (possibleValue == 0x1FF)
+            return ".";
+
+        if (IsOnlyOneBitSet(possibleValue))
+            return ConvertBitwiseToDecimal(possibleValue).ToString();
+
+        string representation = "{";
+
+        for (int i = 1; i < SIZE + 1; i++)
+        {
+            if ((possibleValue & (1 << (i-1))) == 0)
+                continue;
+
+            representation += i.ToString();
+        }
+
+        return representation + "}";
+    }
+
+    private static string CenterString(string s, int width)
+    {
+        if (s.Length >= width)
+        {
+            return s;
+        }
+
+        int leftPadding = (width - s.Length) / 2;
+        int rightPadding = width - s.Length - leftPadding;
+
+        return new string(' ', leftPadding) + s + new string(' ', rightPadding);
+    }
+    
+    public static int ConvertBitwiseToDecimal(short value) => int.TrailingZeroCount(value) + 1;
+    public static bool IsOnlyOneBitSet(short value) => (value & (value - 1)) == 0;
+
     private static void AddRow(int cell, HashSet<int> set)
     {
         int inferiorLimit = cell / SIZE * SIZE;
-        int superiorLimit = cell / SIZE * (SIZE + 1);
+        int superiorLimit = inferiorLimit + SIZE;
         for (int i = inferiorLimit; i < superiorLimit; i++)
             set.Add(i);
     }
     
     private static void AddSquare(int cell, HashSet<int> set)
     {
-        int coordinateX = (cell / SIZE) / 3 * 3;
-        int coordinateY = (cell % SIZE) / 3 * 3;
+        int coordinateY = (cell / SIZE) / 3 * 3;
+        int coordinateX = (cell % SIZE) / 3 * 3;
         int inferiorLimit = coordinateY * SIZE + coordinateX;
         for (int i = 0; i < SIZE; i++)
         {
