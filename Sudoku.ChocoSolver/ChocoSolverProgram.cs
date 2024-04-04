d1586efaa7c65245e35060200da054a2d277516a
Emon Barberis <emon.barberis@epita.fr>
2024-04-04 15:25:21 +0200

    add ChocoSolver

---
+++ Sudoku.ChocoSolver/ChocoSolverProgram.cs
@@ -0,0 +1,63 @@
+﻿using Python.Runtime;
+using System;
+using System.Linq;
+using Sudoku.Shared;
+
+namespace Sudoku.ChocoSolverEngine
+{
+    public class ChocoSolverMethod : ISudokuSolver
+    {
+        public SudokuGrid Solve(SudokuGrid s)
+        {
+            Runtime.PythonDLL = "/root/.pyenv/versions/3.12.0/lib/libpython3.12.so";
+            PythonEngine.Initialize();
+
+            using (Py.GIL())
+            {
+                dynamic sys = Py.Import("sys");
+                string codeBasePath = AppDomain.CurrentDomain.BaseDirectory;
+                string resourcesPath = Path.Combine(codeBasePath, "Resources");
+                sys.path.append(resourcesPath);
+
+                dynamic solver = Py.Import("test");
+
+                PyObject pyInitialGrid = ConvertToPythonList(s);
+                PyObject pySolvedGrid = solver.solve_sudoku(pyInitialGrid);
+
+                return ConvertFromPythonList(pySolvedGrid);
+            }
+        }
+
+        private PyObject ConvertToPythonList(SudokuGrid s)
+        {
+            using (Py.GIL())
+            {
+                var pythonList = new PyList();
+                for (int i = 0; i < 9; i++)
+                {
+                    var rowList = new PyList();
+                    for (int j = 0; j < 9; j++)
+                    {
+                        rowList.Append(new PyInt(s.Cells[i, j]));
+                    }
+                    pythonList.Append(rowList);
+                }
+                return pythonList;
+            }
+        }
+
+        private SudokuGrid ConvertFromPythonList(PyObject solvedGridPython)
+        {
+            SudokuGrid result = new SudokuGrid();
+            for (int i = 0; i < 9; i++)
+            {
+                var pythonRow = solvedGridPython[i];
+                for (int j = 0; j < 9; j++)
+                {
+                    result.Cells[i, j] = pythonRow[j].As<int>();
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Sudoku.ChocoSolver/Resources/test.py
@@ -0,0 +1,42 @@
+from pychoco.model import Model
+
+def solve_sudoku(initial_grid):
+    model = Model("Sudoku Solver")
+
+    grid = [[model.intvar(1, 9, name="cell_{}_{}".format(i, j)) for j in range(9)] for i in range(9)]
+
+    for i in range(9):
+        model.all_different([grid[i][j] for j in range(9)]).post()  # Lignes
+        model.all_different([grid[j][i] for j in range(9)]).post()  # Colonnes
+
+    for block_row in range(3):
+        for block_col in range(3):
+            square = [grid[3 * block_row + i][3 * block_col + j] for i in range(3) for j in range(3)]
+            model.all_different(square).post()
+
+    for i in range(9):
+        for j in range(9):
+            if initial_grid[i][j] != 0:
+                model.arithm(grid[i][j], "=", initial_grid[i][j]).post()
+
+    solver = model.get_solver()
+    if solver.solve():
+        solved_grid = [[grid[i][j].get_value() for j in range(9)] for i in range(9)]
+        return solved_grid
+    else:
+        print("No solution found.")
+        return None
+    
+"""
+initial_grid = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+"""
