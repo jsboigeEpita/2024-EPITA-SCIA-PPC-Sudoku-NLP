3941d1416b6f448a2c02da61b31343274c972047
Jason Perez <jasonperez@mbp-de-jason.home>
2024-04-04 19:09:40 +0200

    Structure commencé

---
+++ Sudoku.GeneticAlgorithm/GeneticSolver.cs
@@ -0,0 +1,11 @@
+﻿namespace Sudoku.Shared
+{
+    public class GeneticSolver : ISudokuSolver
+    {
+        public SudokuGrid Solve(SudokuGrid s)
+        {
+            return s.CloneSudoku();
+        }
+
+    }
+}
