e2b089760d059012509ed8b3c031fe6033ce6b08
samy.hadj-said <samy.hadj-said@epita.fr>
2024-04-04 18:44:33 +0200

    add geneticAlgorithmSolver

---
+++ Sudoku.GeneticAlgorithm/GeneticAlgorithmSolver.cs
@@ -0,0 +1,12 @@
+﻿namespace Sudoku.Shared
+{
+    public class GeneticAlgorithmSolver : ISudokuSolver
+    {
+        public SudokuGrid Solve(SudokuGrid s)
+        {
+            Console.WriteLine("ICIIIIIIIIIIIIIIIII");
+            return s.CloneSudoku();
+        }
+
+    }
+}
