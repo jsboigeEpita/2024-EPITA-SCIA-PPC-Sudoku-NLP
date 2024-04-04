5db7d9647dd9f839b01012873c691c0bfd8c82ed
Florian Tigoulet <flotig77@gmail.com>
2024-04-04 14:19:13 +0200

    fix: cleaned code and .csproj

--- Sudoku.Z3/Class1.cs
+++ Sudoku.Z3/Class1.cs
@@ -16,14 +16,10 @@ namespace SudokuZ3
 {
     public class Z3 : ISudokuSolver
     {
-        public SudokuGrid Solve(SudokuGrid ss)
+        private IntExpr[][] CreateEvalMatrix(Context ctx)
         {
-            Context ctx = new Context(new Dictionary<string, string>() { { "model", "true" } });
-           
-            Console.WriteLine("SudokuExample");
-
-            // 9x9 matrix of integer variables
             IntExpr[][] X = new IntExpr[9][];
+
             for (uint i = 0; i < 9; i++)
             {
                 X[i] = new IntExpr[9];
@@ -31,8 +27,13 @@ namespace SudokuZ3
                     X[i][j] = (IntExpr)ctx.MkConst(ctx.MkSymbol("x_" + (i + 1) + "_" + (j + 1)), ctx.IntSort);
             }
 
-            // each cell contains a value in {1, ..., 9}
+            return X;
+        }
+
+        private Expr[][] EvalCell(Context ctx, IntExpr[][] X)
+        {
             Expr[][] cells_c = new Expr[9][];
+
             for (uint i = 0; i < 9; i++)
             {
                 cells_c[i] = new BoolExpr[9];
@@ -41,13 +42,23 @@ namespace SudokuZ3
                                               ctx.MkLe(X[i][j], ctx.MkInt(9)));
             }
 
-            // each row contains a digit at most once
+            return cells_c;
+        }
+
+        private BoolExpr[] EvalRow(Context ctx, IntExpr[][] X)
+        {
             BoolExpr[] rows_c = new BoolExpr[9];
+
             for (uint i = 0; i < 9; i++)
                 rows_c[i] = ctx.MkDistinct(X[i]);
 
-            // each column contains a digit at most once
+            return rows_c;
+        }
+
+        private BoolExpr[] EvalCol(Context ctx, IntExpr[][] X)
+        {
             BoolExpr[] cols_c = new BoolExpr[9];
+
             for (uint j = 0; j < 9; j++)
             {
                 IntExpr[] column = new IntExpr[9];
@@ -57,8 +68,16 @@ namespace SudokuZ3
                 cols_c[j] = ctx.MkDistinct(column);
             }
 
-            // each 3x3 square contains a digit at most once
+            return cols_c;
+            {
+                
+            }
+        }
+
+        private BoolExpr[][] EvalSquare(Context ctx, IntExpr[][] X)
+        {
             BoolExpr[][] sq_c = new BoolExpr[3][];
+
             for (uint i0 = 0; i0 < 3; i0++)
             {
                 sq_c[i0] = new BoolExpr[3];
@@ -72,29 +91,29 @@ namespace SudokuZ3
                 }
             }
 
+            return sq_c;
+        }
+
+        private BoolExpr EvalSudoku(Context ctx, Expr[][] cells_c, BoolExpr[] rows_c, BoolExpr[] cols_c, BoolExpr[][] sq_c)
+        {
             BoolExpr sudoku_c = ctx.MkTrue();
+
             foreach (BoolExpr[] t in cells_c)
                 sudoku_c = ctx.MkAnd(ctx.MkAnd(t), sudoku_c);
+
             sudoku_c = ctx.MkAnd(ctx.MkAnd(rows_c), sudoku_c);
             sudoku_c = ctx.MkAnd(ctx.MkAnd(cols_c), sudoku_c);
+
             foreach (BoolExpr[] t in sq_c)
                 sudoku_c = ctx.MkAnd(ctx.MkAnd(t), sudoku_c);
 
-            // sudoku instance, we use '0' for empty cells
-            // int[,] instance = {{0,0,0,0,9,4,0,3,0},
-            //                    {0,0,0,5,1,0,0,0,7},
-            //                    {0,8,9,0,0,0,0,4,0},
-            //                    {0,0,0,0,0,0,2,0,8},
-            //                    {0,6,0,2,0,1,0,5,0},
-            //                    {1,0,2,0,0,0,0,0,0},
-            //                    {0,7,0,0,0,0,5,2,0},
-            //                    {9,0,0,0,6,5,0,0,0},
-            //                    {0,4,0,9,7,0,0,0,0}};
-            int[,] instance = ss.Cells;
+            return sudoku_c;
+        }
 
-
-
+        private BoolExpr CreateSudokuToSolve(Context ctx, int[,] instance, IntExpr[][] X)
+        {
             BoolExpr instance_c = ctx.MkTrue();
+
             for (uint i = 0; i < 9; i++)
                 for (uint j = 0; j < 9; j++)
                     instance_c = ctx.MkAnd(instance_c,
@@ -103,36 +122,61 @@ namespace SudokuZ3
                                     ctx.MkTrue(),
                                     ctx.MkEq(X[i][j], ctx.MkInt(instance[i, j]))));
 
-            Solver s = ctx.MkSolver();
-            s.Assert(sudoku_c);
-            s.Assert(instance_c);
-            var res = new SudokuGrid();
-            if (s.Check() == Status.SATISFIABLE)
-            {
-                Model m = s.Model;
-                Expr[,] R = new Expr[9, 9];
-                for (uint i = 0; i < 9; i++)
-                    for (uint j = 0; j < 9; j++)
-                    {
-                        R[i, j] = m.Evaluate(X[i][j]);
-                        res.Cells[i, j] = (int)((IntNum)R[i, j]).UInt64; 
-                    }
-                Console.WriteLine("Sudoku solution:");
-                for (uint i = 0; i < 9; i++)
+            return instance_c;
+        }
+
+        private SudokuGrid SolveSudoku(Solver solver, IntExpr[][] X)
+        {
+            var sudoku_g = new SudokuGrid();
+            Model m = solver.Model;
+            Expr[,] R = new Expr[9, 9];
+
+            for (uint i = 0; i < 9; i++)
+                for (uint j = 0; j < 9; j++)
                 {
-                    for (uint j = 0; j < 9; j++)
-                        Console.Write(" " + R[i, j]);
-                    Console.WriteLine();
+                    R[i, j] = m.Evaluate(X[i][j]);
+                    sudoku_g.Cells[i, j] = (int)((IntNum)R[i, j]).UInt64; 
                 }
-                
-              
-            }
-            else
+
+            return sudoku_g;
+        }
+
+        public SudokuGrid Solve(SudokuGrid s)
+        {
+            Context ctx = new Context(new Dictionary<string, string>() { { "model", "true" } });
+
+            // 9x9 matrix of integer variables
+            IntExpr[][] X = CreateEvalMatrix(ctx);
+
+            // each cell contains a value in {1, ..., 9}
+            Expr[][] cells_c = EvalCell(ctx, X);
+
+            // each row contains a digit at most once
+            BoolExpr[] rows_c = EvalRow(ctx, X);
+
+            // each column contains a digit at most once
+            BoolExpr[] cols_c = EvalCol(ctx, X);
+
+            // each 3x3 square contains a digit at most once
+            BoolExpr[][] sq_c = EvalSquare(ctx, X);
+
+            // evaluate the sudoku with all the previous Evaluations
+            BoolExpr sudoku_c = EvalSudoku(ctx, cells_c, rows_c, cols_c, sq_c);
+
+            int[,] instance = s.Cells;
+
+            BoolExpr instance_c = CreateSudokuToSolve(ctx, instance, X);
+
+            Solver solver = ctx.MkSolver();
+            solver.Assert(sudoku_c);
+            solver.Assert(instance_c);
+
+            if (solver.Check() != Status.SATISFIABLE)
             {
-                Console.WriteLine("Failed to solve sudoku");
+                return s;
             }
             
-            return res;
+            return SolveSudoku(solver, X);
         }
 
     }
