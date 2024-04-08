b8d328bf9afbf664c25266d81170c313b1f297d9
Martin <martin2.natale@epita.fr>
2024-04-08 23:33:33 +0200

    feat: New model for better perfomance

--- Sudoku.NeuralNetworkPVSM/Resources/neuralnetwork.py
+++ Sudoku.NeuralNetworkPVSM/Resources/neuralnetwork.py
@@ -1,66 +1,58 @@
-import copy
 import tensorflow as tf
 import keras
-from keras.layers import Conv2D, BatchNormalization, MaxPooling2D, Flatten, Dense, Reshape, Activation, Dropout
-from keras.callbacks import ReduceLROnPlateau, EarlyStopping
+import tensorflow as tf
+from tensorflow.keras.layers import Reshape, Dense, Dropout, Flatten,Activation
+from tensorflow.keras.layers import Conv1D, Conv2D, BatchNormalization, LayerNormalization, MaxPooling2D
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
 
 def get_data(file):
 
-    data = pd.read_csv(file)
+    df = pd.read_csv(file)
 
-    feat_raw = data['quizzes']
-    label_raw = data['solutions']
+    data = df 
+    X = np.array(df.quizzes.map(lambda x: list(map(int, x))).to_list()).reshape(-1,9,9,1)
+    Y = np.array(df.solutions.map(lambda x: list(map(int, x))).to_list()).reshape(-1,9,9)
+    X = X / 9
+    X -= .5
+    Y -= 1
 
-    feat = []
-    label = []
+    training_split = 0.8
 
-    for i in feat_raw:
-
-        x = np.array([int(j) for j in i]).reshape((9,9,1))
-        feat.append(x)
-
-    feat = np.array(feat)
-    feat = feat/9
-    feat -= .5
-
-    for i in label_raw:
-
-        x = np.array([int(j) for j in i]).reshape((81,1)) - 1
-        label.append(x)
-
-    label = np.array(label)
-
-    del(feat_raw)
-    del(label_raw)
-
-    x_train, x_test, y_train, y_test = train_test_split(feat, label, test_size=0.2, random_state=42)
+    splitidx = int(len(data) * training_split)
+    # first 80% of data as training set, last 20% of data as testing set
+    x_train, x_test = X[:splitidx], X[splitidx:]
+    y_train, y_test = Y[:splitidx], Y[splitidx:]
 
     return x_train, x_test, y_train, y_test
 
 def get_model():
-    model = keras.models.Sequential()
+    model = tf.keras.Sequential()
 
-    model.add(Conv2D(32, kernel_size=(3,3), activation='relu', padding='same', input_shape=(9,9,1)))
+    model.add(Conv2D(512, kernel_size=(3,3), activation='relu', padding='same', input_shape=(9,9,1)))
     model.add(BatchNormalization())
-    model.add(Conv2D(32, kernel_size=(3,3), activation='relu', padding='same'))
+    model.add(Conv2D(512, kernel_size=(3,3), activation='relu', padding='same'))
     model.add(BatchNormalization())
-    model.add(MaxPooling2D(pool_size=(2,2)))
-
-    model.add(Conv2D(64, kernel_size=(3,3), activation='relu', padding='same'))
+    model.add(Conv2D(512, kernel_size=(3,3), activation='relu', padding='same'))
     model.add(BatchNormalization())
-    model.add(Conv2D(64, kernel_size=(3,3), activation='relu', padding='same'))
+    model.add(Conv2D(512, kernel_size=(3,3), activation='relu', padding='same'))
     model.add(BatchNormalization())
-    model.add(MaxPooling2D(pool_size=(2,2)))
-
+    model.add(Conv2D(512, kernel_size=(3,3), activation='relu', padding='same'))
+    model.add(BatchNormalization())
+    model.add(Conv2D(512, kernel_size=(3,3), activation='relu', padding='same'))
+    model.add(BatchNormalization())
+    model.add(Conv2D(512, kernel_size=(3,3), activation='relu', padding='same'))
+    model.add(BatchNormalization())
+    model.add(Conv2D(512, kernel_size=(3,3), activation='relu', padding='same'))
+    model.add(BatchNormalization())
+    model.add(Conv2D(512, kernel_size=(3,3), activation='relu', padding='same'))
     model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
     model.add(Dense(81*9))
-    model.add(Reshape((-1, 9)))
+    model.add(Dropout(0.1))
+    model.add(tf.keras.layers.LayerNormalization(axis=-1))
+    model.add(Reshape((9, 9, 9)))
     model.add(Activation('softmax'))
-
+    
     return model
 
 print(tf.__version__)
@@ -98,89 +90,51 @@ def denorm(a):
     return (a + .5) * 9
 
 
-def inference_sudoku(sample):
-    '''
-        This function solve the sudoku by filling blank positions one by one.
-    '''
-    feat = copy.copy(sample)
-
-    while (1):
-
-        out = model.predict(feat.reshape((1, 9, 9, 1)))
-        out = out.squeeze()
-
-        pred = np.argmax(out, axis=1).reshape((9, 9)) + 1
-        prob = np.around(np.max(out, axis=1).reshape((9, 9)), 2)
-
-        feat = denorm(feat).reshape((9, 9))
-        mask = (feat == 0)
-
-        if (mask.sum() == 0):
-            break
-
-        prob_new = prob * mask
-
-        ind = np.argmax(prob_new)
-        x, y = (ind // 9), (ind % 9)
-
-        val = pred[x][y]
-        feat[x][y] = val
-        feat = norm(feat)
-
-    return pred
-
-
-def test_accuracy(feats, labels):
-    correct = 0
-
-    for i, feat in enumerate(feats):
-
-        pred = inference_sudoku(feat)
-
-        true = labels[i].reshape((9, 9)) + 1
-
-        if (abs(true - pred).sum() == 0):
-            correct += 1
-
-    return(correct / feats.shape[0])
-
-
-def solve_sudoku(game):
-    game = game.replace('\n', '')
-    game = game.replace(' ', '')
-    game = np.array([int(j) for j in game]).reshape((9, 9, 1))
-    game = norm(game)
-    game = inference_sudoku(game)
-    return game
-
 model = get_model()
-train = True
+train = False
 
 if train:
-    x_train, x_test, y_train, y_test = get_data(r'..\..\..\..\Sudoku.NeuralNetworkPVSM\Resources\sudoku.csv')
-    rmsprop = keras.optimizers.RMSprop(learning_rate=0.01)
-    model.compile(loss='sparse_categorical_crossentropy', optimizer=rmsprop)
-
-    reduce_lr = ReduceLROnPlateau(monitor='loss', factor=0.1, patience=5, min_lr=0.00001)
-    early_stop = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
-    model.fit(x_train, y_train, batch_size=128, epochs=20, callbacks=[reduce_lr, early_stop])
-    model.save(r'..\..\..\..\Sudoku.NeuralNetworkPVSM\Resources\model.keras')
-    accuracy = test_accuracy(x_test[:100], y_test[:100])
-    print("Accuracy:", accuracy)
+    x_train, x_test, y_train, y_test = get_data(r'sudoku.csv')
+    optimizer = tf.keras.optimizers.Adam(0.001)
+    model.compile(loss='sparse_categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+    history = model.fit(x_train, y_train, batch_size=64, epochs=5, validation_data=(x_test, y_test))
+    training_accuracy11 = history.history['accuracy'][-1]
+    training_loss11 = history.history['loss'][-1]
+    print(f"Training Loss: {training_loss11:.4f}, Training Accuracy: {training_accuracy11:.4f}")
+    val_loss11, val_accuracy11 = model.evaluate(x_test,y_test)
+    print(f"Done!\nValiation Loss: {val_loss11:.4f}, Validation Accuracy: {val_accuracy11:.4f}")
+    model.save(r'model.keras')
 else:
     try:
-        model = keras.models.load_model(r'..\..\..\..\Sudoku.NeuralNetworkPVSM\Resources\model.keras')
+        model = keras.models.load_model(r'model.keras')
     except Exception as e:
         print("Model not found!")
         print("Please set train=True to train the model before inference.")
         exit()
 
-instance_str = np_instance.flatten()
-instance_str = ''.join(map(str, instance_str))
-instance_str = instance_str.replace('\n', '')
-instance_str = instance_str.replace(' ', '')
-game = solve_sudoku(instance_str)
+def display(board):
+    for i in range(9):
+        if i in [3, 6]:
+            print('------+-------+------')
+        for j in range(9):
+            if j in [3, 6]:
+                print('| ', end='')
+            print(str(board[i][j]) + ' ', end='')  # Convert integer to string before concatenating
+        print()
+
+
+def predict_sudoku(model, sudoku):
+    # Prétraiter le Sudoku pour le rendre compatible avec le modèle
+    sudoku_input = np.array(sudoku).reshape(1, 9, 9, 1) / 9 - 0.5
+    # Faire la prédiction
+    predicted_solution = model.predict(sudoku_input)
+    # Récupérer la solution prédite et la reformater
+    predicted_solution = np.argmax(predicted_solution, axis=-1).reshape(9, 9) + 1
+    return predicted_solution
+
+game = predict_sudoku(model, np_instance)
+display(np_instance)
 print("Sudoku solved:")
-print(game)
+display(game)
 
 result = np.array(list(map(int, game.flatten()))).reshape((9, 9))
